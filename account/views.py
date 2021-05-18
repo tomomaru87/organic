b8558de9da4c6d0d2fff66c8153518f909971b88
tomomaru87 <tikksyou1129@gmail.com>
2021-05-18 16:37:19 +0900

    ログイン

--- account/views.py
+++ account/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from django.views.generic import TemplateView
+from .forms import SignUpForm
+from.forms import activate_user
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+
+class ActivateView(TemplateView):
+    template_name = "registration/activate.html"
+    
+    def get(self, request, uidb64, token, *args, **kwargs):
+        # 認証トークンを検証して、
+        result = activate_user(uidb64, token)
+        # コンテクストのresultにTrue/Falseの結果を渡します。
+        return super().get(request, result=result, **kwargs)
